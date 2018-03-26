cddc956e7468b45ba45e85aefb1def4a01278434
KotiHemant <kotihemant@gmail.com>
2018-03-26 23:38:34 +0530

    Added MergeIntervals

--- InterviewBit/Arrays/FindDuplicateInArray.cpp
+++ InterviewBit/Arrays/FindDuplicateInArray.cpp
@@ -9,20 +9,19 @@
 
 int findDuplicateInArray(const vector<int> &A) {
 	// Explanation: https://stackoverflow.com/a/33336366/6379722
-	if (A.size() > 1) {
-		int slow = A[0];
-		int fast = A[A[0]];
-		while (slow != fast) {
-			slow = A[slow];
-			fast = A[A[fast]];
-		}
-		fast = 0;
-		while (slow != fast) {
-			slow = A[slow];
-			fast = A[fast];
-		}
-		return slow;
+	if (A.size() <= 1) {
+		return -1;
 	}
-	return -1;
+	int slow = A[0], fast = A[A[0]];
+	while (slow != fast) {
+		slow = A[slow];
+		fast = A[A[fast]];
+	}
+	fast = A[0];
+	while (slow != fast) {
+		slow = A[slow];
+		fast = A[fast];
+	}
+	return slow;
 }
 
